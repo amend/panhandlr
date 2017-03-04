d7d3045bb01001984c44ac3ee728b63438a055a9
Andoni Mendoza <andoni.mendoza@gmail.com>
2017-03-04 13:50:41 -0600

    added backend

---
+++ pandhanldr/config.js
@@ -0,0 +1,4 @@
+exports.mongodb = {
+    uri: 'mongodb://localhost/traffic/austin',
+    auto_reconnect: true
+};
---
+++ pandhanldr/routes/index.js
@@ -0,0 +1,5 @@
+exports.init = function(req, res) {
+    console.log('in / about to render index.html');
+
+    res.render('index.html', req);
+};
