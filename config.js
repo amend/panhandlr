2e1162d7872cf414f442234de32b1579b9d4635f
Andoni Mendoza <andoni.mendoza@gmail.com>
2017-07-26 18:19:35 -0700

    going to revert config.js

--- config.js
+++ config.js
@@ -1,12 +1,5 @@
 
-var uri;
-if (process.env.NODE && ~process.env.NODE.indexOf("heroku")) {
-  uri = process.env.MONGODB_URI;
-} else {
-  uri = 'mongodb://localhost/traffic';
-}
-
 exports.mongodb = {
-    'uri': uri,
-    'auto_reconnect': true
+  uri: 'mongodb://localhost/tweets/austin_tweets_label_time',
+  auto_reconnect: true
 };
